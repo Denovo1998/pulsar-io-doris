297e6ac61f2191f9c6834aedcfb87814fb9ba5a7
刘思楠 <liusinan1998@gmail.com>
2021-07-03 10:33:47 +0800

    First commit

---
+++ src/main/java/org/apache/pulsar/io/doris/DorisAbstractSink.java
@@ -0,0 +1,172 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.pulsar.io.doris;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultRedirectStrategy;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.apache.pulsar.functions.api.Record;
+import org.apache.pulsar.io.core.KeyValue;
+import org.apache.pulsar.io.core.Sink;
+import org.apache.pulsar.io.core.SinkContext;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+@Slf4j
+public abstract class DorisAbstractSink<K, V> implements Sink<byte[]> {
+
+    private String loadUrl = "";
+    private Properties properties = new Properties();
+    private DorisSinkConfig dorisSinkConfig;
+
+    /**
+     * Open connector with configuration
+     *
+     * @param config      initialization config
+     * @param sinkContext
+     * @throws Exception IO type exceptions when opening a connector
+     */
+    @Override
+    public void open(Map<String, Object> config, SinkContext sinkContext) throws Exception {
+        dorisSinkConfig = DorisSinkConfig.load(config);
+
+        // TODO:检查参数是否为null或者不合法
+        String doris_host = dorisSinkConfig.getDoris_host();
+        String doris_db = dorisSinkConfig.getDoris_db();
+        String doris_table = dorisSinkConfig.getDoris_table();
+        String doris_user = dorisSinkConfig.getDoris_user();
+        String doris_password = dorisSinkConfig.getDoris_password();
+        String doris_http_port = dorisSinkConfig.getDoris_http_port();
+        // 是否应该ping一下这个ip？
+        Objects.requireNonNull(doris_host, "Doris Host is not set");
+        Objects.requireNonNull(doris_db, "Doris Database is not set");
+        Objects.requireNonNull(doris_table, "Doris Table is not set");
+        Objects.requireNonNull(doris_user, "Doris User is not set");
+        Objects.requireNonNull(doris_password, "Doris Password is not set");
+        // 是否应该查看port是否开启
+        Objects.requireNonNull(doris_http_port, "Doris HTTP Port is not set");
+        // 是否应该mysql jdbc连接一下看能不能联通Doris
+        // 不知道运行时候报错能否显示详细exception，在此处完善校验处理
+
+        // sendData(, dorisSinkConfig);
+    }
+
+    private void sendData(String content, DorisSinkConfig dorisSinkConfig) throws Exception {
+        final String loadUrl = String.format("http://%s:%s/api/%s/%s/_stream_load",
+                dorisSinkConfig.getDoris_host(),
+                dorisSinkConfig.getDoris_http_port(),
+                dorisSinkConfig.getDoris_db(),
+                dorisSinkConfig.getDoris_table());
+
+        final HttpClientBuilder httpClientBuilder = HttpClients
+                .custom()
+                .setRedirectStrategy(new DefaultRedirectStrategy() {
+                    @Override
+                    protected boolean isRedirectable(String method) {
+                        return true;
+                    }
+                });
+
+        try (CloseableHttpClient client = httpClientBuilder.build()) {
+            HttpPut put = new HttpPut(loadUrl);
+            StringEntity entity = new StringEntity(content, "UTF-8");
+            put.setHeader(HttpHeaders.EXPECT, "100-continue");
+            put.setHeader(HttpHeaders.CONTENT_TYPE, "text/plain; charset=UTF-8");
+            put.setHeader(HttpHeaders.AUTHORIZATION, basicAuthHeader(dorisSinkConfig.getDoris_user(),
+                    dorisSinkConfig.getDoris_password()));
+            // the label header is optional, not necessary
+            // use label header can ensure at most once semantics
+            String label = generateLabel();
+            put.setHeader("label", label);
+            put.setEntity(entity);
+
+            try (CloseableHttpResponse response = client.execute(put)) {
+                String loadResult = "";
+                if (response.getEntity() != null) {
+                    loadResult = EntityUtils.toString(response.getEntity());
+                }
+                final int statusCode = response.getStatusLine().getStatusCode();
+                // statusCode 200 just indicates that doris be service is ok, not stream load
+                // you should see the output content to find whether stream load is success
+                if (statusCode != 200) {
+                    throw new IOException(
+                            String.format("Stream load failed, statusCode=%s load result=%s", statusCode, loadResult));
+                }
+                System.out.println(loadResult);
+            }
+        }
+    }
+
+    /**
+     * 生成doris导入任务的标识label
+     *
+     * @return
+     */
+    private String generateLabel() {
+        Calendar calendar = Calendar.getInstance();
+        String label = String.format("pulsar_io_%s%02d%02d_%02d%02d%02d_%s",
+                calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH),
+                calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND),
+                UUID.randomUUID().toString().replaceAll("-", ""));
+        return label;
+    }
+
+    /**
+     * 构建请求头
+     *
+     * @param username
+     * @param password
+     * @return
+     */
+    private String basicAuthHeader(String username, String password) {
+        final String tobeEncode = username + ":" + password;
+        byte[] encoded = Base64.encodeBase64(tobeEncode.getBytes(StandardCharsets.UTF_8));
+        return "Basic " + new String(encoded);
+    }
+
+    /**
+     * Write a message to Sink
+     *
+     * @param record record to write to sink
+     * @throws Exception
+     */
+    @Override
+    public void write(Record<byte[]> record) throws Exception {
+        KeyValue<K, V> keyValue = extractKeyValue(record);
+
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+
+    public abstract KeyValue<K, V> extractKeyValue(Record<byte[]> message);
+}
---
+++ src/main/java/org/apache/pulsar/io/doris/DorisSinkConfig.java
@@ -0,0 +1,105 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.pulsar.io.doris;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.apache.pulsar.io.core.annotations.FieldDoc;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * @author liusinan
+ * @version 1.0.0
+ * @ClassName DorisSinkConfig.java
+ * @Description doris config
+ * curl --location-trusted -u root -H "label:123" -H "where: k1=20180601" -T testData http://host:port/api/testDb/testTbl/_stream_load
+ * @createTime 2021年07月02日 19:04:00
+ */
+@Data
+@Accessors(chain = true)
+public class DorisSinkConfig implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @FieldDoc(
+        required = true,
+        defaultValue = "xxx.com",
+        help =
+            "A comma-separated list of host and port pairs that are the addresses of "
+          + "the Kafka brokers that a Kafka client connects to initially bootstrap itself")
+    private String doris_host;
+
+    @FieldDoc(
+        required = true,
+        defaultValue = "",
+        help =
+            "A comma-separated list of host and port pairs that are the addresses of "
+          + "the Kafka brokers that a Kafka client connects to initially bootstrap itself")
+    private String doris_db;
+
+    @FieldDoc(
+        required = true,
+        defaultValue = "",
+        help =
+            "A comma-separated list of host and port pairs that are the addresses of "
+          + "the Kafka brokers that a Kafka client connects to initially bootstrap itself")
+    private String doris_table;
+
+    @FieldDoc(
+        required = true,
+        defaultValue = "root",
+        help =
+            "A comma-separated list of host and port pairs that are the addresses of "
+          + "the Kafka brokers that a Kafka client connects to initially bootstrap itself")
+    private String doris_user;
+
+    @FieldDoc(
+        required = true,
+        defaultValue = "",
+        help =
+            "A comma-separated list of host and port pairs that are the addresses of "
+          + "the Kafka brokers that a Kafka client connects to initially bootstrap itself")
+    private String doris_password;
+
+    @FieldDoc(
+        required = true,
+        defaultValue = "8410",
+        help =
+            "A comma-separated list of host and port pairs that are the addresses of "
+          + "the Kafka brokers that a Kafka client connects to initially bootstrap itself")
+    private String doris_http_port;
+
+    public static DorisSinkConfig load(String yamlFile) throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        return mapper.readValue(new File(yamlFile), DorisSinkConfig.class);
+    }
+
+    public static DorisSinkConfig load(Map<String, Object> map) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(new ObjectMapper().writeValueAsString(map), DorisSinkConfig.class);
+    }
+
+}
