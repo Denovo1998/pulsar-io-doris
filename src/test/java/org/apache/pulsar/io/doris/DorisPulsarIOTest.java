e9a1bce91491117fe35752199364c28fd2eeac68
刘思楠 <liusinan1998@gmail.com>
2021-07-12 18:51:19 +0800

    v0.2修复pom.xml并添加生产消息测试用例

--- src/test/java/org/apache/pulsar/io/doris/DorisPulsarIOTest.java
+++ src/test/java/org/apache/pulsar/io/doris/DorisPulsarIOTest.java
@@ -20,25 +20,32 @@
 package org.apache.pulsar.io.doris;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.pulsar.client.api.Message;
-import org.apache.pulsar.client.api.schema.GenericSchema;
-import org.apache.pulsar.client.api.schema.SchemaDefinition;
+import org.apache.pulsar.client.api.*;
+import org.apache.pulsar.client.api.schema.*;
 import org.apache.pulsar.client.impl.MessageImpl;
 import org.apache.pulsar.client.impl.schema.JSONSchema;
 import org.apache.pulsar.client.impl.schema.generic.GenericJsonRecord;
 import org.apache.pulsar.client.impl.schema.generic.GenericJsonSchema;
+import org.apache.pulsar.common.schema.SchemaInfo;
+import org.apache.pulsar.common.schema.SchemaType;
 import org.apache.pulsar.common.util.ObjectMapperFactory;
 import org.apache.pulsar.functions.api.Record;
 import org.apache.pulsar.functions.source.PulsarRecord;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -80,7 +87,7 @@ public class DorisPulsarIOTest {
 
     @Test
     public void testSendData() throws Exception {
-        Message<GenericJsonRecord> insertMessage = mock(MessageImpl.class);
+        /*Message<GenericJsonRecord> insertMessage = mock(MessageImpl.class);
 
         JSONSchema<StreamTest> jsonSchema =
                 JSONSchema.of(SchemaDefinition.<StreamTest>builder().withPojo(StreamTest.class).build());
@@ -92,6 +99,7 @@ public class DorisPulsarIOTest {
         streamTest.setUsername("username-1");
 
         JsonNode jsonNode = ObjectMapperFactory.getThreadLocal().valueToTree(streamTest);
+        // GenericJsonRecord非public，解决方法是把代码粘贴到pulsar的源代码中，添加module。
         GenericJsonRecord genericJsonRecord =
                 new GenericJsonRecord(null, null, jsonNode, genericJsonSchema.getSchemaInfo());
 
@@ -103,12 +111,67 @@ public class DorisPulsarIOTest {
                 .build();
 
         when(insertMessage.getValue()).thenReturn(genericJsonRecord);
-        // when(insertMessage.getProperties()).thenReturn(actionProperties);
 
         log.info("Message.getValue: {}, record.getValue: {}", insertMessage.getValue().toString(),
                 insertRecord.getValue().toString());
         dorisGenericJsonRecordSink.write(insertRecord);
         log.info("executed write");
-        future.get(2, TimeUnit.SECONDS);
+        future.get(2, TimeUnit.SECONDS);*/
+    }
+
+    private static final String TOPIC = "doris-sink";
+
+    @Test
+    public void testProduceMessage() {
+        final String pulsarServiceUrl = "pulsar://localhost:6650";
+        try (PulsarClient client = PulsarClient.builder()
+                .serviceUrl(pulsarServiceUrl)
+                .build()) {
+            RecordSchemaBuilder schemaBuilder = SchemaBuilder.record(
+                    "io.streamnative.examples.schema.json"
+            );
+            schemaBuilder.field("id")
+                    .type(SchemaType.INT64)
+                    .required();
+            schemaBuilder.field("id2")
+                    .type(SchemaType.INT64)
+                    .required();
+            schemaBuilder.field("username")
+                    .type(SchemaType.STRING)
+                    .required();
+            SchemaInfo schemaInfo = schemaBuilder.build(SchemaType.JSON);
+            GenericSchema<GenericRecord> schema = Schema.generic(schemaInfo);
+            try (Producer<GenericRecord> producer = client.newProducer(schema)
+                    .topic(TOPIC)
+                    .create()) {
+                final int numMessages = 1000;
+                for (long i = 0L; i < numMessages; i++) {
+                    final long id = i;
+                    final long id2 = i + 1L;
+                    String username = "user-" + i;
+                    GenericRecord record = schema.newRecordBuilder()
+                            .set("id", id)
+                            .set("id2", id2)
+                            .set("username", username)
+                            .build();
+                    // send the payment in an async way
+                    producer.newMessage()
+                            .key(username)
+                            .value(record)
+                            .sendAsync();
+                    if (i % 100 == 0) {
+                        Thread.sleep(200);
+                    }
+                }
+                // flush out all outstanding messages
+                producer.flush();
+                System.out.printf("Successfully produced %d messages to a topic called %s%n",
+                        numMessages, TOPIC);
+            }
+        } catch (PulsarClientException | InterruptedException e) {
+            System.err.println("Failed to produce generic avro messages to pulsar:");
+            e.printStackTrace();
+            Runtime.getRuntime().exit(-1);
+        }
     }
 }
